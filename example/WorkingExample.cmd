14842f81031e2df11681eb72df71bc7f39b9d8ef
Davis Robinson <davispr13@gmail.com>
2023-03-24 18:22:14 -0400

    adding working example files for ease of use, ability to execute from terminal/cmd, and just some general code cleanup

---
+++ example/WorkingExample.cmd
@@ -0,0 +1,4 @@
+:: Example of executing from windows cmd
+python calc_4_param_weibull.py -i example/token_data.txt
+:: Example of executing from windows cmd with an output file
+python calc_4_param_weibull.py -i example/token_data.txt -o weibull_pred_values.txt -x example/plot_val_at.txt
---
+++ example/WorkingExample.sh
@@ -0,0 +1,2 @@
+# Example of executing from linux terminal
+python3 calc_4_param_weibull.py -i example/token_data.txt
